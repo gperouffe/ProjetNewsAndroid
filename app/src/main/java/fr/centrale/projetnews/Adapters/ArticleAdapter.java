e55cdea604ca03dda5893eedcb3b11baec4e87b8
gperouffe <g.perouffe@gmail.com>
2017-11-25 15:40:38 +0100

    Utilisation de CardViews pour liste articles

--- app/src/main/java/fr/centrale/projetnews/Adapters/ArticleAdapter.java
+++ app/src/main/java/fr/centrale/projetnews/Adapters/ArticleAdapter.java
@@ -1,20 +1,19 @@
 package fr.centrale.projetnews.Adapters;
 
 import android.content.Context;
-import android.support.annotation.LayoutRes;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.Locale;
 
 import fr.centrale.projetnews.POJO.NewsArticle;
 import fr.centrale.projetnews.R;
@@ -27,6 +26,9 @@ import fr.centrale.projetnews.Utils.Consts;
 public class ArticleAdapter extends RecyclerView.Adapter<ArticleAdapter.ViewHolder> implements View.OnClickListener{
 
     private ArrayList<NewsArticle> articles;
+    private SimpleDateFormat dateParse = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
+    private DateFormat toLocaleDate = DateFormat.getDateInstance(DateFormat.LONG, Locale.FRANCE);
+    private DateFormat toLocaleTime = DateFormat.getTimeInstance( DateFormat.SHORT, Locale.FRANCE);
 
     public ArticleAdapter(ArrayList<NewsArticle> articles) {
         Log.d(Consts.TAG, articles.size() + "");
@@ -35,10 +37,14 @@ public class ArticleAdapter extends RecyclerView.Adapter<ArticleAdapter.ViewHold
 
     public static class ViewHolder extends RecyclerView.ViewHolder{
         TextView title;
+        TextView date;
+        TextView author;
 
         public ViewHolder(View itemView) {
             super(itemView);
             title = itemView.findViewById(R.id.article_title);
+            date = itemView.findViewById(R.id.article_date);
+            author = itemView.findViewById(R.id.article_author);
         }
     }
 
@@ -52,6 +58,14 @@ public class ArticleAdapter extends RecyclerView.Adapter<ArticleAdapter.ViewHold
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
         holder.title.setText(articles.get(position).getTitle());
+
+        try {
+            Date published = dateParse.parse(articles.get(position).getPublishedAt());
+            holder.date.setText("Publié le " + toLocaleDate.format(published) + " à " + toLocaleTime.format(published));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        holder.author.setText(articles.get(position).getAuthor());
     }
 
     @Override
